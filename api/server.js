2965407d08e7a5728496ab46bfe562ac959a5413
Maytê <maytearaujo@yahoo.com.br>
2025-04-17 19:49:44 -0300

    feat: Correcao dos enderecos

--- api/server.js
+++ api/server.js
@@ -1,11 +1,18 @@
 const jsonServer = require("json-server");
-const path = require("path");
-
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "../data/plants.json"));
+const router = jsonServer.router("data/plants.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.use((req, res, next) => {
+  // Permitir todos os métodos
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
 server.use(router);
 
 module.exports = server;
